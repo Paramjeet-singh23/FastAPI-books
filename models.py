4d8828c1b94dc1b203ba7e9142eb6c916850cd5d
paramjeetsingh <Paramjeet9933@gmail.com>
2023-05-16 23:06:59 +0530

    change database to mysql

--- models.py
+++ models.py
@@ -1,24 +1,24 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, VARCHAR
 from database import Base
 
 class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    password = Column(String)
+    email = Column(String(50), unique=True, )
+    username = Column(String(50), unique=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    password = Column(String(50))
     is_active = Column(Boolean, default=True)
-    role = Column(String)
+    role = Column(String(50))
 
 class Todos(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
-    priority = Column(String)
+    title = Column(String(50))
+    description = Column(String(50))
+    priority = Column(String(50))
     complete = Column(Boolean, default=False)
     owner_id = Column(Integer, ForeignKey("users.id"))
