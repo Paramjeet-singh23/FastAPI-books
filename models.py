c9501a357da266865457e839102c27a21706374d
paramjeetsingh <Paramjeet9933@gmail.com>
2023-05-16 23:11:54 +0530

    data length change

--- models.py
+++ models.py
@@ -5,20 +5,20 @@ class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(50), unique=True, )
-    username = Column(String(50), unique=True)
-    first_name = Column(String(50))
-    last_name = Column(String(50))
-    password = Column(String(50))
+    email = Column(String(200), unique=True, )
+    username = Column(String(200), unique=True)
+    first_name = Column(String(200))
+    last_name = Column(String(200))
+    password = Column(String(500))
     is_active = Column(Boolean, default=True)
-    role = Column(String(50))
+    role = Column(String(200))
 
 class Todos(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(50))
-    description = Column(String(50))
-    priority = Column(String(50))
+    title = Column(String(200))
+    description = Column(String(200))
+    priority = Column(String(200))
     complete = Column(Boolean, default=False)
     owner_id = Column(Integer, ForeignKey("users.id"))
